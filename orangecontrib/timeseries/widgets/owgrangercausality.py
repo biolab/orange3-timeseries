64e2c80452e483e77131f72e0e4c5f0235838818
Kernc <kerncece@gmail.com>
2016-05-31 00:24:47 +0200

    OWGrangerCausality: don't output distmatrix (doesn't play well); just show a table

--- orangecontrib/timeseries/widgets/owgrangercausality.py
+++ orangecontrib/timeseries/widgets/owgrangercausality.py
@@ -1,18 +1,14 @@
-import numpy as np
+from PyQt4.QtCore import Qt
 
 from Orange.data import Table, Domain
-from Orange.misc import DistMatrix
 from Orange.widgets import widget, gui, settings
+from Orange.widgets.utils.itemmodels import PyTableModel
 from orangecontrib.timeseries import Timeseries
 
 # TODO: use VAR Granger causality
 # http://statsmodels.sourceforge.net/devel/generated/statsmodels.tsa.vector_ar.var_model.VARResults.test_causality.html
 # http://statsmodels.sourceforge.net/devel/vector_ar.html#granger-causality
 from statsmodels.tsa.stattools import grangercausalitytests
-
-
-class Output:
-    LAGS = 'Lags matrix'
 
 
 class OWGrangerCausality(widget.OWWidget):
@@ -23,42 +19,45 @@ class OWGrangerCausality(widget.OWWidget):
     priority = 190
 
     inputs = [("Timeseries", Timeseries, 'set_data')]
-    outputs = [(Output.LAGS, DistMatrix)]
-
-    want_main_area = False
-    resizing_enabled = False
 
     max_lag = settings.Setting(20)
     confidence = settings.Setting(95)
     autocommit = settings.Setting(False)
 
     UserAdviceMessages = [
-        widget.Message('This widget outputs a distance matrix. The values '
-                       'represent the minimum lag for which "row '
-                       'Granger-causes column" holds true, or 0 if no such lag.',
-                       'output-distance-matrix',
-                       widget.Message.Warning),
         widget.Message('We say <i>X</i> Granger-causes <i>Y</i> if '
                        'predictions of values of <i>Y</i> based on its own '
                        'past values and on the past values of <i>X</i> are '
                        'better than predictions of <i>Y</i> based on its '
-                       'past values alone.',
-                       'output-distance-matrix',
+                       'past values alone.<br><br>'
+                       'It does NOT mean <i>X</i> causes <i>Y</i>!',
+                       'explanation',
                        widget.Message.Warning)
     ]
 
     def __init__(self):
         self.data = None
-        box = gui.vBox(self.controlArea, 'Parameters')
+        box = gui.vBox(self.controlArea, 'Granger Test')
         gui.hSlider(box, self, 'confidence',
                     minValue=90, maxValue=99,
                     label='Confidence:',
                     labelFormat=" %d%%",
                     callback=self.on_changed)
         gui.spin(box, self, 'max_lag', 1, 50,
-                 label='Maximum lag:',
+                 label='Max lag:',
                  callback=self.on_changed)
-        gui.auto_commit(box, self, 'autocommit', '&Apply')
+        gui.auto_commit(box, self, 'autocommit', '&Test')
+        gui.rubber(self.controlArea)
+
+        self.model = model = PyTableModel(parent=self)
+        model.setHorizontalHeaderLabels(['Min. lag', 'Series 1', '', 'Series 2'])
+        view = gui.TableView(self)
+        view.setModel(model)
+        bold = view.BoldFontDelegate(self)
+        view.setItemDelegateForColumn(1, bold)
+        view.setItemDelegateForColumn(3, bold)
+        view.horizontalHeader().setStretchLastSection(False)
+        self.mainArea.layout().addWidget(view)
 
     def on_changed(self):
         self.commit()
@@ -69,18 +68,18 @@ class OWGrangerCausality(widget.OWWidget):
 
     def commit(self):
         data = self.data
+        self.model.clear()
+        self.error()
         if data is None:
-            self.send(Output.LAGS, None)
+            return
 
         res = []
         data = data.interp()
         alpha = 1 - self.confidence / 100
         domain = [var for var in data.domain if var.is_continuous]
-        self.error()
         try:
             with self.progressBar(len(domain)**2) as progress:
                 for row_attr in domain:
-                    row = []
                     for col_attr in domain:
                         if row_attr == col_attr or data.time_variable in (row_attr, col_attr):
                             continue
@@ -88,15 +87,14 @@ class OWGrangerCausality(widget.OWWidget):
                         tests = grangercausalitytests(X, self.max_lag, verbose=False)
                         lag = next((lag for lag in range(1, 1 + self.max_lag)
                                     if tests[lag][0]['ssr_ftest'][1] < alpha), 0)
-                        row.append(lag)
+                        if lag:
+                            res.append([lag, row_attr.name, '→', col_attr.name])
                         progress.advance()
-                    res.append(row)
         except ValueError as ex:
             self.error(ex.args[0])
         else:
-            lags = DistMatrix(np.asarray(res, dtype=float),
-                              data.domain, data.domain)
-            self.send(Output.LAGS, lags)
+            self.model.wrap(res)
+            self.model.sort(0, Qt.DescendingOrder)
 
 
 if __name__ == "__main__":
