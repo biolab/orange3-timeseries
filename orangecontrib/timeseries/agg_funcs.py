b550abd989f0001d1af3f7023a1c873daa58d85b
Kernc <kerncece@gmail.com>
2016-06-02 22:10:04 +0200

    Add OWMovingTransform widget

---
+++ orangecontrib/timeseries/agg_funcs.py
@@ -0,0 +1,107 @@
+import numpy as np
+from scipy.stats import mode, hmean, gmean
+
+
+class _AggFuncMeta(type):
+    def __call__(cls, array):
+        return cls.__func__(array)
+
+    def __str__(cls):
+        return cls.__name__.replace('_', ' ')
+
+
+class _AggFunc(metaclass=_AggFuncMeta):
+    """
+    This class makes below aggregation functions callable but also
+    nicely convertable to str for presentation and use in e.g.
+    PyListModel or PyTableModel.
+    """
+    #: Override this
+    __func__ = None
+
+
+class Sum(_AggFunc):
+    __func__ = np.nansum
+
+
+class Product(_AggFunc):
+    __func__ = np.nanprod
+
+
+class Mean(_AggFunc):
+    __func__ = np.nanmean
+
+
+class Count_nonzero(_AggFunc):
+    __func__ = lambda arr: np.count_nonzero(arr[~np.isnan(arr)])
+
+
+class Max(_AggFunc):
+    __func__ = np.nanmax
+
+
+class Min(_AggFunc):
+    __func__ = np.nanmin
+
+
+class Mean(_AggFunc):
+    __func__ = np.nanmean
+
+
+class Median(_AggFunc):
+    __func__ = np.nanmedian
+
+
+class Std_deviation(_AggFunc):
+    __func__ = np.nanstd
+
+
+class Variance(_AggFunc):
+    __func__ = np.nanvar
+
+
+class Mode(_AggFunc):
+    __func__ = lambda arr: mode(arr, nan_policy='omit').mode[0]
+
+
+class Harmonic_mean(_AggFunc):
+    __func__ = lambda arr: hmean(arr[arr > 0], axis=None)
+
+
+class Geometric_mean(_AggFunc):
+    __func__ = lambda arr: gmean(arr[np.logical_and(~np.isnan(arr), (arr != 0))], axis=None)
+
+
+class Weighted_MA(_AggFunc):
+    def __func__(arr):
+        l = len(arr)
+        w = np.arange(l, 0, -1) / (l * (l + 1) / 2)
+        return (w * arr).sum()
+
+
+class Exponential_MA(_AggFunc):
+    def __func__(arr):
+        # Window length is fixed (len(arr)) and the last element is considered
+        # to add 1% to the total value. When this is inverted ...
+        alpha = 1 - np.exp(np.log(.01) / (len(arr) - 1))
+        w = (1 - alpha)**np.arange(len(arr))
+        w /= w.sum()
+        return (w * arr).sum()
+
+
+AGG_FUNCTIONS = [
+    Mean,
+    Sum,
+    Max,
+    Min,
+    Median,
+    Mode,
+    Std_deviation,
+    Variance,
+    Product,
+    Weighted_MA,
+    Exponential_MA,
+    Harmonic_mean,
+    Geometric_mean,
+    Count_nonzero,
+]
---
+++ orangecontrib/timeseries/widgets/owmovingtransform.py
@@ -0,0 +1,255 @@
+import numpy as np
+from PyQt4.QtCore import Qt, QSize
+from PyQt4.QtGui import QIcon, QStyledItemDelegate, QComboBox, QSpinBox
+
+from Orange.data import Domain, ContinuousVariable
+from Orange.widgets import widget, gui, settings
+from Orange.widgets.utils.itemmodels import VariableListModel, PyListModel, PyTableModel
+
+from orangecontrib.timeseries import Timeseries
+from orangecontrib.timeseries.agg_funcs import AGG_FUNCTIONS, Mean
+from orangecontrib.timeseries.widgets.utils import available_name
+
+
+class Output:
+    TIMESERIES = 'Time series'
+
+
+class OWMovingTransform(widget.OWWidget):
+    name = 'Moving Transform'
+    description = 'Apply rolling window functions to the time series.'
+    icon = 'icons/MovingTransform.svg'
+    priority = 20
+
+    inputs = [("Time series", Timeseries, 'set_data')]
+    outputs = [("Time series", Timeseries)]
+
+    want_main_area = False
+    resizing_enabled = False
+
+    non_overlapping = settings.Setting(False)
+    fixed_wlen = settings.Setting(5)
+    transformations = settings.Setting([])
+    autocommit = settings.Setting(False)
+    last_win_width = settings.Setting(5)
+
+    _NON_OVERLAPPING_WINDOWS = 'Non-overlapping windows'
+
+    UserAdviceMessages = [
+        widget.Message('Get the simple moving average (SMA) of a series '
+                       'by setting the aggregation function to "{}".'.format(Mean),
+                       'sma-is-mean'),
+        widget.Message('If "{}" is checked, the rolling windows don\t '
+                       'overlap. Instead, they run through the series '
+                       'side-to-side, so the resulting transformed series is '
+                       'fixed-window-length-times shorter.'.format(_NON_OVERLAPPING_WINDOWS),
+                       'non-overlapping')
+    ]
+
+    def __init__(self):
+        self.data = None
+        box = gui.vBox(self.controlArea, 'Moving Transform')
+
+        def _disable_fixed_wlen():
+            fixed_wlen.setDisabled(not self.non_overlapping)
+            self.view.repaint()
+            self.on_changed()
+
+        gui.checkBox(box, self, 'non_overlapping',
+                     label=self._NON_OVERLAPPING_WINDOWS,
+                     callback=_disable_fixed_wlen,
+                     tooltip='If this is checked, instead of rolling windows '
+                             'through the series, they are applied side-to-side, '
+                             'so the resulting output series will be some '
+                             'length-of-fixed-window-times shorter.')
+        fixed_wlen = gui.spin(box, self, 'fixed_wlen', 2, 1000,
+                              label='Fixed window width:',
+                              callback=self.on_changed)
+        fixed_wlen.setDisabled(not self.non_overlapping)
+
+        class TableView(gui.TableView):
+            def __init__(self, parent, agg_functions):
+                super().__init__(parent,
+                                 editTriggers=(self.SelectedClicked |
+                                               self.CurrentChanged |
+                                               self.DoubleClicked |
+                                               self.EditKeyPressed),
+                                 )
+                self.horizontalHeader().setStretchLastSection(False)
+                agg_functions = PyListModel(agg_functions, parent=self)
+                self.setItemDelegateForColumn(0, self.VariableDelegate(parent))
+                self.setItemDelegateForColumn(1, self.SpinDelegate(parent))
+                self.setItemDelegateForColumn(2, self.ComboDelegate(self, agg_functions))
+
+            class _ItemDelegate(QStyledItemDelegate):
+                def updateEditorGeometry(self, widget, option, _index):
+                    widget.setGeometry(option.rect)
+
+            class ComboDelegate(_ItemDelegate):
+                def __init__(self, parent=None, combo_model=None):
+                    super().__init__(parent)
+                    self._parent = parent
+                    if combo_model is not None:
+                        self._combo_model = combo_model
+
+                def createEditor(self, parent, _QStyleOptionViewItem, index):
+                    combo = QComboBox(parent)
+                    combo.setModel(self._combo_model)
+                    return combo
+
+                def setEditorData(self, combo, index):
+                    var = index.model().data(index, Qt.EditRole)
+                    combo.setCurrentIndex(self._combo_model.indexOf(var))
+
+                def setModelData(self, combo, model, index):
+                    var = self._combo_model[combo.currentIndex()]
+                    model.setData(index, var, Qt.EditRole)
+
+            class VariableDelegate(ComboDelegate):
+                @property
+                def _combo_model(self):
+                    return self._parent.var_model
+
+            class SpinDelegate(_ItemDelegate):
+                def paint(self, painter, option, index):
+                    # Don't paint window length if non-overlapping windows set
+                    if not self.parent().non_overlapping:
+                        super().paint(painter, option, index)
+
+                def createEditor(self, parent, _QStyleOptionViewItem, _index):
+                    # Don't edit window length if non-overlapping windows set
+                    if self.parent().non_overlapping:
+                        return None
+                    spin = QSpinBox(parent, minimum=1, maximum=1000)
+                    return spin
+
+                def setEditorData(self, spin, index):
+                    spin.setValue(index.model().data(index, Qt.EditRole))
+
+                def setModelData(self, spin, model, index):
+                    spin.interpretText()
+                    model.setData(index, spin.value(), Qt.EditRole)
+
+        self.var_model = VariableListModel(parent=self)
+
+        self.table_model = model = PyTableModel(self.transformations,
+                                                parent=self, editable=True)
+        model.setHorizontalHeaderLabels(['Series', 'Window width', 'Aggregation function'])
+        model.dataChanged.connect(self.on_changed)
+
+        self.view = view = TableView(self, AGG_FUNCTIONS)
+        view.setModel(model)
+        box.layout().addWidget(view)
+
+        hbox = gui.hBox(box)
+        from os.path import dirname, join
+        self.add_button = button = gui.button(
+            hbox, self, 'Add &Transform',
+            callback=self.on_add_transform)
+        button.setIcon(QIcon(join(dirname(__file__), 'icons', 'LineChart-plus.png')))
+
+        self.del_button = button = gui.button(
+            hbox, self, '&Delete Selected',
+            callback=self.on_del_transform)
+        QIcon.setThemeName('gnome')  # Works for me
+        button.setIcon(QIcon.fromTheme('edit-delete'))
+
+        gui.auto_commit(box, self, 'autocommit', '&Apply')
+
+    def sizeHint(self):
+        return QSize(450, 600)
+
+    def on_add_transform(self):
+        if self.data:
+            self.table_model.append([self.var_model[0], self.last_win_width, AGG_FUNCTIONS[0]])
+        self.commit()
+
+    def on_del_transform(self):
+        for row in sorted([mi.row() for mi in self.view.selectionModel().selectedRows(0)],
+                          reverse=True):
+            del self.table_model[row]
+        if len(self.table_model):
+            selection_model = self.view.selectionModel()
+            selection_model.select(self.table_model.index(len(self.table_model) - 1, 0),
+                                   selection_model.Select | selection_model.Rows)
+        self.commit()
+
+    def set_data(self, data):
+        self.data = data
+        self.add_button.setDisabled(not len(getattr(data, 'domain', ())))
+        self.table_model.clear()
+        if data is not None:
+            self.var_model.wrap([var for var in data.domain
+                                 if var.is_continuous and var is not data.time_variable])
+        self.on_changed()
+
+    def on_changed(self):
+        self.commit()
+
+    def commit(self):
+        data = self.data
+        if not data:
+            self.send(Output.TIMESERIES, None)
+            return
+
+        X = []
+        attrs = []
+        no_overlap = self.non_overlapping
+        for var, wlen, func in self.table_model:
+            col = np.ravel(data[:, var])
+
+            if no_overlap:
+                wlen = self.fixed_wlen
+
+            # In reverse cause lazy brain. Also tend to have informative ends, not beginnings as much
+            col = col[::-1]
+
+            out = [func(col[i:i + wlen])
+                   for i in range(0,
+                                  len(col) - wlen + int(no_overlap),
+                                  wlen if no_overlap else 1)]
+            # The first few convolution results are incomplete / values unknown
+            if not no_overlap:
+                out.extend([np.nan] * wlen)
+
+            out = np.array(out)[::-1]
+
+            X.append(out)
+
+            template = '{} ({}; {})'.format(var.name, wlen, func)
+            name = available_name(data.domain, template)
+            attrs.append(ContinuousVariable(name))
+
+        dataX, dataY, dataM = data.X, data.Y, data.metas
+        if self.non_overlapping:
+            n = len(X[0])
+            dataX = dataX[::-1][::self.fixed_wlen][:n][::-1]
+            dataY = dataY[::-1][::self.fixed_wlen][:n][::-1]
+            dataM = dataM[::-1][::self.fixed_wlen][:n][::-1]
+
+        ts = Timeseries(Domain(data.domain.attributes + tuple(attrs),
+                               data.domain.class_vars,
+                               data.domain.metas),
+                        np.column_stack((dataX, np.column_stack(X))) if X else dataX,
+                        dataY, dataM)
+        ts.time_variable = data.time_variable
+        self.send(Output.TIMESERIES, ts)
+
+
+if __name__ == "__main__":
+    from PyQt4.QtGui import QApplication
+
+    a = QApplication([])
+    ow = OWMovingTransform()
+
+    data = Timeseries('yahoo_MSFT')
+    attrs = [var.name for var in data.domain.attributes]
+    if 'Adj Close' in attrs:
+        # Make Adjusted Close a class variable
+        attrs.remove('Adj Close')
+        data = Timeseries(Domain(attrs, [data.domain['Adj Close']], None, source=data.domain), data)
+
+    ow.set_data(data)
+
+    ow.show()
+    a.exec()
