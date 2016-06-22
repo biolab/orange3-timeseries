d3106d4f16659734bce0b2897d319d36055e8b9a
Kernc <kerncece@gmail.com>
2016-06-22 19:38:15 +0200

    Add datasets dir, install it into Orange's datasets dir

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
+import os
+import sys
+import pkg_resources
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 VERSION = '0.1.0'
 
@@ -23,25 +27,55 @@ ENTRY_POINTS = {
     ),
 }
 
+
+class LinkDatasets(install):
+    def run(self):
+        super().run()
+
+        old_cwd = os.getcwd()
+        os.chdir(os.path.abspath(os.path.sep))
+
+        src = pkg_resources.resource_filename('orangecontrib.timeseries', 'datasets')
+        dst = os.path.join(pkg_resources.resource_filename('Orange', 'datasets'), 'timeseries')
+
+        try:
+            os.remove(dst)
+        except OSError:
+            pass
+        try:
+            os.symlink(src, dst, target_is_directory=True)
+        except OSError:
+            pass
+        finally:
+            os.chdir(old_cwd)
+
+
+
 if __name__ == '__main__':
     setup(
         name="Orange3-Timeseries",
         description="Orange add-on for exploring time series and sequential data.",
         version=VERSION,
         author='Bioinformatics Laboratory, FRI UL',
-        author_email='contact@orange.biolab.si',
+        author_email='info@biolab.si',
         url='https://github.com/biolab/orange3-timeseries',
         keywords=(
             'time series',
             'sequence analysis',
             'orange3 add-on',
+            'ARIMA',
+            'VAR model',
+            'forecast'
         ),
+        cmdclass={'install': LinkDatasets},
         packages=find_packages(),
         package_data={
             "orangecontrib.timeseries.widgets": ["icons/*.svg"],
+            "orangecontrib.timeseries": ["datasets/*.tab",
+                                         "datasets/*.csv"],
         },
         install_requires=[
-            'Orange',
+            'Orange3',
             'statsmodels>=0.6.1',
             'numpy',
             'scipy',
