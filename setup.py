7734dbd6e4238f5bced87f897d17ec02cbc4fdc9
Kernc <kerncece@gmail.com>
2017-01-27 14:52:40 +0100

    setup.py: bump version to 0.2.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import pkg_resources
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = '0.2.5'
+VERSION = '0.2.6'
 
 ENTRY_POINTS = {
     'orange3.addon': (
