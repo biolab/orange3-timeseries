16788bfee37b05620e334313fe599fd09b65e316
Jernej Urankar <jerneju@gmail.com>
2017-12-19 10:05:54 +0100

    [FIX] Model Evaluation: do not crash on a bad model

---
+++ orangecontrib/timeseries/widgets/tests/test_owmodelevaluation.py
@@ -0,0 +1,33 @@
+import unittest
+
+from Orange.data import Table
+from Orange.widgets.tests.base import WidgetTest
+
+from orangecontrib.timeseries import Timeseries, ARIMA
+from orangecontrib.timeseries.widgets.owmodelevaluation import OWModelEvaluation
+
+
+class TestOWModelEvaluation(WidgetTest):
+    def setUp(self):
+        self.widget = self.create_widget(OWModelEvaluation)  # type: OWModelEvaluation
+
+    def test_bad_model(self):
+        """
+        Do not fail on a bad model.
+        GH-37
+        """
+        w = self.widget
+        table = Table("housing")
+        time_series = Timeseries(table)
+        model = ARIMA((2, 5, 1), 0)
+        self.assertFalse(w.Warning.model_not_appropriate.is_shown())
+        self.send_signal(w.Inputs.time_series, time_series)
+        self.send_signal(w.Inputs.time_series_model, model, 0)
+        w.controls.autocommit.click()
+        self.assertTrue(w.Warning.model_not_appropriate.is_shown())
+        self.send_signal(w.Inputs.time_series_model, None, 0)
+        self.assertFalse(w.Warning.model_not_appropriate.is_shown())
+
+
+if __name__ == "__main__":
+    unittest.main()
