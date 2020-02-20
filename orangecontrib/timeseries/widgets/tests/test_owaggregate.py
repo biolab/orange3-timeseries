1bd9e9f8b76587e18c620072bb6f1362347489e3
Ajda Pretnar <ajdapretnar@gmail.com>
2020-02-20 13:58:07 +0100

    Aggregate: test

--- orangecontrib/timeseries/widgets/tests/test_owaggregate.py
+++ orangecontrib/timeseries/widgets/tests/test_owaggregate.py
@@ -33,5 +33,26 @@ class TestOWAggregate(WidgetTest):
         output = self.get_output(w.Outputs.time_series)
         self.assertEqual(len(output.metas.shape), 2)
 
+    def test_no_datetime(self):
+        """ Raise error if no data with TimeVariable """
+        w = self.widget
+        time_series = Timeseries.from_file("airpassengers")
+        table = Table.from_file('iris')
+        self.send_signal(w.Inputs.time_series, time_series)
+        self.assertFalse(w.Error.no_time_variable.is_shown())
+        self.send_signal(w.Inputs.time_series, table)
+        self.assertTrue(w.Error.no_time_variable.is_shown())
+        self.send_signal(w.Inputs.time_series, time_series)
+        self.assertFalse(w.Error.no_time_variable.is_shown())
+
+    def test_tz_aggregation(self):
+        w = self.widget
+        data = Timeseries.from_file('airpassengers')[:20]
+        self.send_signal(w.Inputs.time_series, data)
+        w.controls.autocommit.click()
+        output = self.get_output(w.Outputs.time_series)
+        self.assertEqual(output[0][0], "1949-01-01")
+
+
 if __name__ == "__main__":
     unittest.main()
