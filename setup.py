49d864820d05cb9011494bdf3f9691cf6c701ba9
Ajda Pretnar <ajdapretnar@gmail.com>
2020-03-26 13:17:52 +0100

    Release 0.3.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION = '0.3.4'
+VERSION = '0.3.5'
 
 README_FILE = os.path.join(os.path.dirname(__file__), 'README.pypi')
 LONG_DESCRIPTION = open(README_FILE).read()
