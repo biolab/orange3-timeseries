78f3310e5c10afffe3de8b9e9078616ad8a854df
janezd <janez.demsar@fri.uni-lj.si>
2022-05-29 00:05:03 +0200

    Yahoo Finance: Cosmetic changes

--- orangecontrib/timeseries/widgets/owyahoofinance.py
+++ orangecontrib/timeseries/widgets/owyahoofinance.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timedelta, date
 
 from AnyQt.QtCore import QDate
-from AnyQt.QtWidgets import QDateEdit, QComboBox
+from AnyQt.QtWidgets import QDateEdit, QComboBox, QFormLayout
+
+from orangewidget.utils.widgetpreview import WidgetPreview
 
 from Orange.widgets import widget, gui, settings
 from Orange.widgets.widget import Output
@@ -26,57 +28,45 @@ class OWYahooFinance(widget.OWWidget):
     date_from = settings.Setting(
         (datetime.now().date() - timedelta(5 * 365)).strftime(PY_DATE_FORMAT))
     date_to = settings.Setting(datetime.now().date().strftime(PY_DATE_FORMAT))
-    symbols = settings.Setting(['AMZN', 'AAPL', 'GOOG', 'FB', 'SPY', '^DJI', '^TNX'])
+    symbols = settings.Setting(
+        ['AMZN', 'AAPL', 'GOOG', 'FB', 'SPY', '^DJI', '^TNX'])
 
     want_main_area = False
     resizing_enabled = False
 
     class Error(widget.OWWidget.Error):
-        download_error = widget.Msg('Failed to download data (HTTP Error {}). '
-                                    'Wrong stock symbol?')
+        download_error = widget.Msg('Failed to download data.\n'
+                                    'No internet? Wrong stock symbol?')
 
     def __init__(self):
-        box = gui.widgetBox(self.controlArea, 'Yahoo Finance Stock Data',
-                            orientation='horizontal')
-        lbox = gui.widgetBox(box, orientation='vertical')
-        hbox = gui.widgetBox(lbox, orientation='horizontal')
-        gui.label(hbox, self, 'Ticker:')
-        self.combo = combo = QComboBox(editable=True,
-                                       insertPolicy=QComboBox.InsertAtTop)
+        layout = QFormLayout()
+        gui.widgetBox(self.controlArea, True, orientation=layout)
+
+        self.combo = combo = QComboBox(
+            editable=True, insertPolicy=QComboBox.InsertAtTop)
         combo.addItems(self.symbols)
-        hbox.layout().addWidget(combo)
-        # combo = gui.comboBox(
-        #     lbox, self, 'symbol',#, items=self.symbols,
-        #     label='Ticker:', orientation='horizontal',
-        #     editable=True, maximumContentsLength=-1)
-        gui.rubber(combo.parentWidget())
+        layout.addRow("Ticker:", self.combo)
         minDate = QDate.fromString(self.MIN_DATE.strftime(self.PY_DATE_FORMAT),
                                    self.QT_DATE_FORMAT)
-        date_from = QDateEdit(
-            QDate.fromString(self.date_from, self.QT_DATE_FORMAT),
-            displayFormat=self.QT_DATE_FORMAT,
-            minimumDate=minDate,
-            calendarPopup=True)
-        date_to = QDateEdit(
-            QDate.fromString(self.date_to, self.QT_DATE_FORMAT),
-            displayFormat=self.QT_DATE_FORMAT,
-            minimumDate=minDate,
-            calendarPopup=True)
-        date_from.dateChanged.connect(
-            lambda date: setattr(self, 'date_from',
-                                 date.toString(self.QT_DATE_FORMAT)))
-        date_to.dateChanged.connect(
-            lambda date: setattr(self, 'date_to',
-                                 date.toString(self.QT_DATE_FORMAT)))
-        hbox = gui.hBox(lbox)
-        gui.label(hbox, self, "From:")
-        hbox.layout().addWidget(date_from)
-        hbox = gui.hBox(lbox)
-        gui.label(hbox, self, "To:")
-        hbox.layout().addWidget(date_to)
+        date_from, date_to = (
+            QDateEdit(QDate.fromString(date, self.QT_DATE_FORMAT),
+                      displayFormat=self.QT_DATE_FORMAT, minimumDate=minDate,
+                      calendarPopup=True)
+            for date in (self.date_from, self.date_to))
 
-        self.button = gui.button(self.controlArea, self, 'Download',
-                                 callback=self.download)
+        @date_from.dateChanged.connect
+        def set_date_from(date):
+            self.date_from = date.toString(self.QT_DATE_FORMAT)
+
+        @date_to.dateChanged.connect
+        def set_date_to(date):
+            self.date_to = date.toString(self.QT_DATE_FORMAT)
+
+        layout.addRow("From:", date_from)
+        layout.addRow("To:", date_to)
+
+        self.button = gui.button(
+            self.controlArea, self, 'Download', callback=self.download)
 
     def download(self):
         date_from = datetime.strptime(self.date_from, self.PY_DATE_FORMAT)
@@ -105,16 +95,10 @@ class OWYahooFinance(widget.OWWidget):
 
                 self.Outputs.time_series.send(data)
             except Exception as e:
-                self.Error.download_error(getattr(e, 'status', -1))
+                self.Error.download_error()
             finally:
                 self.button.setDisabled(False)
 
 
 if __name__ == "__main__":
-    from AnyQt.QtWidgets import QApplication
-
-    a = QApplication([])
-    ow = OWYahooFinance()
-
-    ow.show()
-    a.exec()
+    WidgetPreview(OWYahooFinance).run()
