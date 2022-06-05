872205d1967f8304430399e49cb89de22f14ebba
Janez Demšar <janez.demsar@gmail.com>
2022-06-05 19:40:55 +0200

    Revert "ARIMA, Var: Minor refactoring and GUI update"

--- orangecontrib/timeseries/widgets/owvarmodel.py
+++ orangecontrib/timeseries/widgets/owvarmodel.py
@@ -1,7 +1,6 @@
-from AnyQt.QtCore import Qt
+from collections import OrderedDict
 
-from orangewidget.utils.widgetpreview import WidgetPreview
-from Orange.widgets import gui, settings
+from Orange.widgets import widget, gui, settings
 from orangecontrib.timeseries import Timeseries, VAR
 from orangecontrib.timeseries.widgets._owmodel import OWBaseModel
 
@@ -16,36 +15,37 @@ class OWVARModel(OWBaseModel):
     ic = settings.Setting(0)
     trend = settings.Setting(0)
 
-    IC_LABELS = dict((('None', None),
-                      ("Akaike's information criterion (AIC)", 'aic'),
-                      ('Bayesian information criterion (BIC)', 'bic'),
-                      ('Hannan–Quinn', 'hqic'),
-                      ("Final prediction error (FPE)", 'fpe'),
-                      ('Average of the above', 'magic')))
-    TREND_LABELS = dict((('None', 'n'),
-                         ('Constant', 'c'),
-                         ('Constant and linear', 'ct'),
-                         ('Constant, linear and quadratic', 'ctt')))
+    UserAdviceMessages = [
+        widget.Message('Note, VAR model requires the series to be stationary.',
+                       'stationary',
+                       widget.Message.Warning)
+    ]
+
+    IC_LABELS = OrderedDict((('None', None),
+                             ("Akaike's information criterion (AIC)", 'aic'),
+                             ('Bayesian information criterion (BIC)', 'bic'),
+                             ('Hannan–Quinn', 'hqic'),
+                             ("Final prediction error (FPE)", 'fpe'),
+                             ('Average of the above', 'magic')))
+    TREND_LABELS = OrderedDict((('None', 'nc'),
+                                ('Constant', 'c'),
+                                ('Constant and linear', 'ct'),
+                                ('Constant, linear and quadratic', 'ctt')))
 
     def add_main_layout(self):
         box = gui.vBox(self.controlArea, box='Parameters')
-        gui.spin(
-            box, self, 'maxlags', 1, 100,
-            label='Maximum auto-regression order:', alignment=Qt.AlignRight,
-            callback=self.apply.deferred)
-        gui.separator(self.controlArea, 12)
-        box = gui.vBox(self.controlArea, box=True)
+        gui.spin(box, self, 'maxlags', 1, 100, label='Maximum auto-regression order:',
+                 callback=self.apply)
         gui.radioButtons(
             box, self, 'ic',
-            btnLabels=tuple(self.IC_LABELS),
-            label='Optimize AR order by:',
-            callback=self.apply.deferred)
-        gui.separator(self.controlArea, 12)
+            btnLabels=tuple(self.IC_LABELS.keys()),
+            box='Information criterion', label='Optimize AR order by:',
+            callback=self.apply)
         gui.radioButtons(
             box, self, 'trend',
-            btnLabels=tuple(self.TREND_LABELS),
-            label='Add trend vector(s):',
-            callback=self.apply.deferred)
+            btnLabels=tuple(self.TREND_LABELS.keys()),
+            box='Trend', label='Add trend vector(s):',
+            callback=self.apply)
 
     def create_learner(self):
         ic = self.IC_LABELS[tuple(self.IC_LABELS.keys())[self.ic]]
@@ -54,5 +54,13 @@ class OWVARModel(OWBaseModel):
 
 
 if __name__ == "__main__":
+    from AnyQt.QtWidgets import QApplication
+
+    a = QApplication([])
+    ow = OWVARModel()
+
     data = Timeseries.from_file('airpassengers')
-    WidgetPreview(OWVARModel).run(set_data=data)
+    ow.set_data(data)
+
+    ow.show()
+    a.exec()
