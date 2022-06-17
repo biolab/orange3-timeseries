ac73c5972b79ef97c0ff8765f44485909e614a44
Ajda <ajdapretnar@users.noreply.github.com>
2022-06-17 09:49:46 +0200

    Release 0.4.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION = '0.4.0'
+VERSION = '0.4.1'
 
 README_FILE = os.path.join(os.path.dirname(__file__), 'README.pypi')
 LONG_DESCRIPTION = open(README_FILE, encoding='utf-8').read()
