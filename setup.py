7e5401932dd1a332f2e8542feebe1ebf000876e1
PrimozGodec <p.godec9@gmail.com>
2023-03-24 09:31:30 +0100

    Release version 0.5.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION = '0.5.2'
+VERSION = '0.5.3'
 
 README_FILE = os.path.join(os.path.dirname(__file__), 'README.pypi')
 LONG_DESCRIPTION = open(README_FILE, encoding='utf-8').read()
