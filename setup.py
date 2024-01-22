019b9baf5177ac52c8053fcf9b22ab3d31211ac2
PrimozGodec <p.godec9@gmail.com>
2024-01-22 12:56:45 +0100

    Release version 0.6.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION = '0.6.1'
+VERSION = '0.6.2'
 
 README_FILE = os.path.join(os.path.dirname(__file__), 'README.pypi')
 LONG_DESCRIPTION = open(README_FILE, encoding='utf-8').read()
